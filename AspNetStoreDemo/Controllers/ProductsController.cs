2e52b03a48145467e3c808125fe0482f7f3939d3
Ray Cooper <rcooper1337@gmail.com>
2018-12-13 22:41:33 -0600

    Added new controller for the admin section

---
+++ AspNetStoreDemo/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetStoreDemo.Data;
+using AspNetStoreDemo.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetStoreDemo.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductViewModel ProductsVM { get; set; }
+
+        public ProductsController(ApplicationDbContext db)
+        {
+            _db = db;
+            ProductsVM = new ProductViewModel()
+            {
+                ProductTypes = _db.ProductTypes.ToList(),
+                SpecialTags = _db.SpecialTags.ToList(),
+                Products = new Models.Products()
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = _db.Products.Include(m => m.ProductTypes).Include(m => m.SpecialTags);
+
+            return View(await products.ToListAsync());
+        }
+    }
+}
--- AspNetStoreDemo/Data/Migrations/20181214040830_addProducts.Designer.cs
+++ AspNetStoreDemo/Data/Migrations/20181214040830_addProducts.Designer.cs
@@ -4,14 +4,16 @@ using AspNetStoreDemo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AspNetStoreDemo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181214040830_addProducts")]
+    partial class addProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
